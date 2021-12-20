0fa9a0954ce4d43535693b9f1800bd6649d16e08
Ky Huynh <hgky95@gmail.com>
2021-12-20 17:05:17 +0700

    get limit value from configuration file

--- limits-service/src/main/java/com/demo/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/demo/limitsservice/controller/LimitsController.java
@@ -1,15 +1,20 @@
 package com.demo.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.limitsservice.bean.Limits;
+import com.demo.limitsservice.configuration.Configuration;
 
 @RestController
 public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
 
 	@GetMapping("/limits")
 	public Limits getLimits() {
-		return new Limits(1, 1000);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
 	}
 }
