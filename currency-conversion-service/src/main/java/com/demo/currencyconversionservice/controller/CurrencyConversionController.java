f3f8c530bceb510d7192101400bc46419225357e
Ky Huynh <hgky95@gmail.com>
2021-12-24 06:27:48 +0700

    init currency conversion

---
+++ currency-conversion-service/src/main/java/com/demo/currencyconversionservice/controller/CurrencyConversionController.java
@@ -0,0 +1,34 @@
+package com.demo.currencyconversionservice.controller;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.demo.currencyconversionservice.model.CurrencyConversion;
+
+@RestController
+public class CurrencyConversionController {
+
+	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion getCurrencyConversion(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		HashMap<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+
+		CurrencyConversion currencyConversion = responseEntity.getBody();
+		return new CurrencyConversion(currencyConversion.getId(), currencyConversion.getFrom(), currencyConversion.getTo(),
+				quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment());
+
+	}
+}
