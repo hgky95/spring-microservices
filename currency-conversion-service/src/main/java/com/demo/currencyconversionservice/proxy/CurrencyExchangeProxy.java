a689637cb70206515a19dc3baa2259c02a7632c5
Ky Huynh <hgky95@gmail.com>
2021-12-26 22:14:22 +0700

    using feign to call rest api

--- currency-conversion-service/src/main/java/com/demo/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/demo/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.demo.currencyconversionservice.model.CurrencyConversion;
 
-@FeignClient(name="currency-exchange1232", url="localhost:8000")
+//@FeignClient(name="currency-exchange1232", url="localhost:8000")
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
